84d6446b97c9294e00d6c61baa51fe1cb0de8397
조수연 <tndus8334@naver.com>
2025-09-19 16:34:10 +0900

    디비구축테스트

--- BackEnd/hackerton/src/main/java/com/example/hackerton/mapper/FestivalLogMapper.java
+++ BackEnd/hackerton/src/main/java/com/example/hackerton/mapper/FestivalLogMapper.java
@@ -2,8 +2,11 @@ package com.example.hackerton.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.hackerton.domain.FestivalLog;
 
+@Mapper
 public interface FestivalLogMapper {
     List<FestivalLog> findRecentPredictions();
     void insertPredictionLog(FestivalLog log);
--- BackEnd/hackerton/src/main/java/com/example/hackerton/mapper/FestivalMapper.java
+++ BackEnd/hackerton/src/main/java/com/example/hackerton/mapper/FestivalMapper.java
@@ -2,8 +2,11 @@ package com.example.hackerton.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.hackerton.domain.Festival;
 
+@Mapper
 public interface FestivalMapper {
     List<Festival> findAll();
     Festival findById(Long festivalId);
